ed1dac0ec2d31fe892523e7e9921dd5fd8040081
angelina <floresangela.0707@gmail.com>
2022-03-24 10:04:48 +0600

    feat: add solution to transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let actions = [
+    '--discard-next',
+    '--double-next',
+    '--discard-prev',
+    '--double-prev',
+  ];
+  
+  let numbers = arr.slice().filter((item) => !actions.includes(item));
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === actions[0] && i != arr.length - 1) {
+      numbers.splice(i, 1);
+    } else if (arr[i] === actions[1] && i != arr.length - 1) {
+      if (numbers.includes(arr[i + 1])) {
+        numbers.splice(i, 0, arr[i + 1]);
+      }
+    } else if (arr[i] === actions[2] && i != 0) {
+      if (numbers.includes(arr[i - 1])) {
+        numbers.splice(i - 1, 1);
+      }
+    } else if (arr[i] === actions[3] && i != 0) {
+      if (numbers.includes(arr[i - 1])) {
+        numbers.splice(i, 0, arr[i - 1]);
+      }
+    }
+  }
+
+  return numbers;
 }
+
 
 module.exports = {
   transform
