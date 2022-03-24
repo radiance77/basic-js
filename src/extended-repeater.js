f22ca642bc570dc058683214a1ac1a17dc42b15c
angelina <floresangela.0707@gmail.com>
2022-03-24 09:59:32 +0600

    feat: add solution to extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+  str = String(str);
+  if ('addition' in options) {
+    options.addition = String(options.addition);
+  }
+  if (!('separator' in options)) {
+    options.separator = '+';
+  }
+  if (!('additionSeparator' in options)) {
+    options.additionSeparator = '|';
+  }
+  if (!('additionRepeatTimes' in options)) {
+    options.additionRepeatTimes = 1;
+  }
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    if('addition' in options) {
+      str += options.addition;
+    }
+    if (i !== options.additionRepeatTimes - 1) {
+      str += options.additionSeparator;
+    }
+  }
+
+  if ('repeatTimes' in options) {
+    let word = str;
+    for (let i = 0; i < options.repeatTimes - 1; i++) {
+      str += options.separator;
+      str += word;
+    }
+  }
+  return str;
 }
 
 module.exports = {
